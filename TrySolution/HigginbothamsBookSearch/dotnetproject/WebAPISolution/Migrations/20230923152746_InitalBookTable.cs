96c37c727f703064f38fb46cb413291ab3493d5d
Iamneo <techadmin@iamneo.ai>
2023-09-26 12:39:08 +0000

    Backup Commit

---
+++ TrySolution/HigginbothamsBookSearch/dotnetproject/WebAPISolution/Migrations/20230923152746_InitalBookTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStoreDBFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitalBookTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    author = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    quantity = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Book__3213E83F97675A18", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
